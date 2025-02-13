4127b8c5efb9294e0f076fbc807b0448fa53dc87
Akshat Pratyush <akshatp0505@gmail.com>
2025-02-13 22:14:49 +0530

    Added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Projects from './components/Projects'
+import TechStack from './components/TechStack'
 
 const App = () => {
   return (
