a09edc8814d5714316f94b6e16d474db35920da7
Akshat Pratyush <akshatp0505@gmail.com>
2025-03-06 23:27:09 +0530

    Created project list

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,27 +1,46 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const ContactForm = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const sendMessage = () => {
+
+    };
+
     return (
         <div className="flex justify-center items-center bg-black px-4">
             <div className="w-full max-w-lg bg-black p-8 rounded-lg">
-                <form className="space-y-4">
+                <form onSubmit={handleSubmit(sendMessage)} className="space-y-4">
                     <input
                         type="text"
                         placeholder="Name"
+                        {...register("name", { required: "Name is required" })}
+                        required
                         className="w-full p-3 bg-black text-white border border-gray-600 rounded-md focus:outline-none focus:border-gray-400"
                     />
                     <input
                         type="email"
                         placeholder="Email"
+                        required
+                        {...register("email", { required: "Email is required" })}
                         className="w-full p-3 bg-black text-white border border-gray-600 rounded-md focus:outline-none focus:border-gray-400"
                     />
                     <input
                         type="text"
                         placeholder="Subject"
+                        required
+                        {...register("subject", { required: "Subject is required" })}
                         className="w-full p-3 bg-black text-white border border-gray-600 rounded-md focus:outline-none focus:border-gray-400"
                     />
                     <textarea
                         placeholder="Message"
+                        required
+                        {...register("message", { required: "Message is required" })}
                         className="w-full p-3 bg-black text-white border border-gray-600 rounded-md focus:outline-none focus:border-gray-400 h-32 resize-none overflow-hidden"
                     ></textarea>
                     <button
---
+++ src/data/projects.js
@@ -0,0 +1,44 @@
+//Template for the projects object
+// {
+//     id: 1,
+//     name: '',
+//     description: "",
+//     domain: '',
+//     tech: '',
+//     thumbnail: '../assets/projects/',
+//     github: '',
+//     projectLink: ''
+//   },
+
+export const projects = [{
+    id: 0,
+    name: 'Block-Carnate',
+    description: "Developed a single-player 2D platformer game in Godot. The game is a set of levels which all requires the player's out of box thinking to clear them. The game requires the player to die at a certain place to solve each puzzle of the level by instantiating an interactable block at the place of death and use it to move further ahead.",
+    domain: 'Game Dev',
+    tech: 'GDScript',
+    thumbnail: '../assets/projects/blockcarnate.png',
+    github: 'https://github.com/akshatp17/block-carnate',
+    projectLink: 'https://infamix-studios.itch.io/blockcarnate'
+  },
+  {
+    id: 1,
+    name: 'Blood Bank Management',
+    description: "Developed a Frontend of a Blood Bank Management System as a first-year mini-project to streamline blood donations. The prototype allows users to check blood stock, track donors, locate nearby banks, and schedule donations, prioritizing usability and accessibility",
+    domain: 'Web Dev',
+    tech: 'HTML, CSS, JS',
+    thumbnail: '../assets/projects/bloodbank.png',
+    github: 'https://github.com/akshatp17/BloodBankMiniProject',
+    projectLink: 'https://akshatp17.github.io/BloodBankMiniProject/'
+  },
+  {
+    id: 2,
+    name: 'Tic-Tac-Toe',
+    description: "Developed an interactive tic-tac-toe website allowing players to compete with the other players locally",
+    domain: 'Web Dev',
+    tech: 'Reactjs',
+    thumbnail: '../assets/projects/',
+    github: 'https://github.com/akshatp17/TicTacToe',
+    projectLink: 'https://akshatp17.github.io/TicTacToe/'
+  },
+];
+  
