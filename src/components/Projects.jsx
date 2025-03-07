dc5727c8247f777e70289c09ce6f82fc033b802f
Akshat Pratyush <akshatp0505@gmail.com>
2025-03-07 21:17:31 +0530

    Fixed project items squeezing issue

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Projects = ({ project }) => {
     return (
         <div
-            className="flex flex-col w-50 h-40 rounded-4xl p-5 bg-cover bg-center"
+            className="flex flex-col min-w-50 min-h-40 rounded-4xl p-5 bg-cover bg-center bg-no-repeat"
             style={{ backgroundImage: `url(${project.thumbnail})` }}
         >
             <h2 className="text-lg font-bold text-black">{project.name}</h2>
