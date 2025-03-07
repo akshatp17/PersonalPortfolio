41bc3643b04a4342dd3d8914527b45eef050ca70
Akshat Pratyush <akshatp0505@gmail.com>
2025-03-07 12:09:27 +0530

    minor fix

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ExperienceCard = (props) => {
     return (
-        <div className='font-bold w-32 h-48 rounded-4xl flex flex-col justify-center items-center px-5 py-auto inset-shadow-black ring-4 ring-white'>
+        <div className='font-bold w-32 h-48 rounded-4xl flex flex-col justify-center items-center px-5 py-auto inset-ring-4 inset-ring-white'>
             <p className='text-5xl flex justify-center items-center'>{props.expData.num}<span className='text-4xl'>+</span></p>
             <p className='text-center'>{props.expData.numContext}</p>
         </div>
