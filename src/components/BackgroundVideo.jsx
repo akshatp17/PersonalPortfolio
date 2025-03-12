295a302cd957a66167cc12a06d9b5cbd3fffe63c
Akshat Pratyush <akshatp0505@gmail.com>
2025-03-12 21:38:41 +0530

    Fixed bg video

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from "react";
 import backgroundVideo from "/bg.mp4";
 
 const BackgroundVideo = () => {
     return (
-        <video className='videoTag -z-50 fixed' autoPlay loop muted>
-            <source src={backgroundVideo} type='video/mp4' />
+        <video
+            className="fixed top-0 left-0 w-full h-full object-cover -z-50"
+            autoPlay
+            loop
+            muted
+        >
+            <source src={backgroundVideo} type="video/mp4" />
         </video>
-    )
-}
+    );
+};
 
-export default BackgroundVideo
+export default BackgroundVideo;
